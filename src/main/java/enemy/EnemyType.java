14fa2808658d616103b5da440624bbf1953af6f5
oshi192 <yuriykoss@gmail.com>
2018-04-10 14:23:05 +0300

    dev: coment EnemyType

--- src/main/java/enemy/EnemyType.java
+++ src/main/java/enemy/EnemyType.java
@@ -1,7 +1,7 @@
 package enemy;
 
 public enum EnemyType {
-    T11("UNDEAD", "Skeleton", 10, 40, 6, 5),
+    /*T11("UNDEAD", "Skeleton", 10, 40, 6, 5),
     T12("UNDEAD", "Sceleton Chief", 15, 60, 6, 10),
     T13("UNDEAD", "Zombie", 5, 80, 6, 15),
     T14("UNDEAD", "Banshee", 15, 75, 10, 18),
@@ -12,14 +12,14 @@ public enum EnemyType {
     T23("ORKOIDS", "Ork Shaman", 35, 80, 26, 22),
     T24("ORKOIDS", "Ork Warlord", 50, 150, 26, 40),
     T25("ORKOIDS", "Ogre", 5, 300, 21, 100),
-    T26("ORKOIDS", "Unstable Alchemist", 25, 15, 1, 5);/*
+    T26("ORKOIDS", "Unstable Alchemist", 25, 15, 1, 5);
     T31("UNDEAD","Skeleton",10,40,6,5),
     T32("UNDEAD","Sceleton Chief",15,60,6,10),
     T33("UNDEAD","Zombie",5,80,6,15),
     T34("UNDEAD","Banshee",15,75,10,18),
     T35("UNDEAD","Litch",20,100,26,25),
-    T36("UNDEAD","Unstable Alchemist",50,550,31,300),*/
-    private String race;
+    T36("UNDEAD","Unstable Alchemist",50,550,31,300),
+    private  String race;
     private String type;
     private int initiative;
     private float hp;
@@ -27,11 +27,12 @@ public enum EnemyType {
     private int cost;
 
     EnemyType(String race, String type, int initiative, float hp, int damage, int cost) {
-        this.race = race;
-        this.type = type;
-        this.initiative = initiative;
-        this.hp = hp;
-        this.damage = damage;
-        this.cost = cost;
+        this.race=race;
+        this.type=type;
+        this.initiative=initiative;
+        this.hp=hp;
+        this.damage=damage;
+        this.cost=cost;
     }
+    */
 }

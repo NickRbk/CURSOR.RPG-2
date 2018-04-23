f9a4cb772dd62d31a8e0a309a6dbed22ffc4428f
Nick Rybak <nrybak94@gmail.com>
2018-04-23 23:42:11 +0300

    task#1: delete all code with wrong maze implementation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,10 @@
-import labyrinth.Dungeon;
-
 import util.*;
 
 class Game implements Colors, GameConstants ,displayingInfo{
     private Team team;
-    private Dungeon dungeon;
 
     Game() {
         team = new Team();
-        dungeon = new Dungeon();
     }
 
     void start(){
@@ -17,17 +13,5 @@ class Game implements Colors, GameConstants ,displayingInfo{
         System.out.println(" ------------------------------");
         Console.printParagraph(GAME_DESCRIPTION);
         team.createTeam();
-        turn();
     }
-
-    private void turn() {
-        team.setPosition(dungeon.getRooms().get(0));
-        String s = team.getPosition().getName();
-        while (!"Exit".equals(s)) {
-            s = team.getPosition().getName();
-            team.setPosition(dungeon.chooseNextRoom(team.getPosition()));
-        }
-    }
-
-
 }

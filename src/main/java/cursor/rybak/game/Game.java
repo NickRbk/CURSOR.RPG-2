f76ab20c78c81093fdf882c8d29b67ef88f511ce
Nick Rybak <nrybak94@gmail.com>
2018-04-24 18:12:29 +0300

    task#2_team: fully rewrite Team class, because of previous mistakes

--- src/main/java/cursor/rybak/game/Game.java
+++ src/main/java/cursor/rybak/game/Game.java
@@ -1,15 +1,5 @@
 package cursor.rybak.game;
 
-import cursor.rybak.model.Team;
-
 public class Game {
-    private Team team;
 
-    public Game() {
-        team = new Team();
-    }
-
-    public void start(){
-        team.createTeam();
-    }
 }
---
+++ src/main/java/cursor/rybak/model/team/Team.java
@@ -0,0 +1,54 @@
+package cursor.rybak.model.team;
+
+import cursor.rybak.game.UserInteraction;
+import cursor.rybak.model.race.AbstractRace;
+import lombok.Getter;
+
+
+public class Team {
+    private static final int TEAM_MEMBERS = 3;
+
+    @Getter
+    private AbstractRace[] heroes;
+    @Getter
+    private String name;
+
+    // Create custom constructor to create Team
+    public Team(String name) {
+        heroes = createTeam();
+        this.name = name;
+    }
+
+
+    /**
+     * create Team
+     *
+     * @return array of members team
+     */
+    private AbstractRace[] createTeam() {
+        AbstractRace[] selectedHeroes = UserInteraction.askHeroes(TEAM_MEMBERS);
+        selectedHeroes[0].setLeader();
+        return selectedHeroes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\n\t%s team, members: %d\n\t\t--> %s",
+                name, heroes.length, printTeamMembers());
+    }
+
+    /**
+     * return String of all members name in team
+     *
+     * @return members name in team
+     */
+    private String printTeamMembers() {
+        String[] heroesName = new String[heroes.length];
+
+        for (int i = 0; i < heroes.length; i++) {
+            heroesName[i] = String.format("%s (%.0f)", heroes[i].getHeroName(), heroes[i].getHealth());
+        }
+
+        return String.join(", ", heroesName);
+    }
+}

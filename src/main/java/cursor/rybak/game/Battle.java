fd70afe6c48ed7a91e64c484e50806df14a54515
Nick Rybak <nrybak94@gmail.com>
2018-04-28 16:36:40 +0300

    task#4_enemies: add msg about enemies, when team enter to room

--- src/main/java/cursor/rybak/game/Battle.java
+++ src/main/java/cursor/rybak/game/Battle.java
@@ -7,6 +7,13 @@ public class Battle {
     public static void blitzkrieg(Room currentRoom) {
         if(currentRoom.isMonsterPresent()) {
             BattleMessage.printStartBattleInfo();
+            StringBuilder enemies = new StringBuilder();
+
+            currentRoom.getEnemies()
+                    .forEach(enemy -> enemies.append(String.format("%s(%d)  ", enemy.getName(), enemy.getHealth())));
+
+            System.out.println(enemies.toString());
+
             currentRoom.setMonsterPresent(0);
         }
     }
--- src/main/java/cursor/rybak/view/BattleMessage.java
+++ src/main/java/cursor/rybak/view/BattleMessage.java
@@ -2,6 +2,6 @@ package cursor.rybak.view;
 
 public class BattleMessage implements MagicColors {
     public static void printStartBattleInfo() {
-        System.out.println(RED + "\t<===BATTLE PHASE WILL BE HERE===>" + RESET);
+        System.out.print(RED + "\t<===BATTLE PHASE WILL BE HERE===>" + RESET);
     }
 }

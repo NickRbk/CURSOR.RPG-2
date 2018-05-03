090cc055ad6653d20e106a59df2d46f159a859b7
Nick Rybak <nrybak94@gmail.com>
2018-05-03 16:44:40 +0300

    dev: update mosters count props

--- src/main/java/cursor/rybak/store/enemies/count/EnemiesCount.java
+++ src/main/java/cursor/rybak/store/enemies/count/EnemiesCount.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum EnemiesCount {
-    LVL_1(1, 3), LVL_3(3, 4), LVL_5(5, 4), LVL_7(7, 5), LVL_9(9, 4);
+    LVL_1(1, 3), LVL_3(3, 4), LVL_5(5, 5), LVL_7(7, 6), LVL_9(9, 7);
 
     private int minLevel;
     private int totalCount;

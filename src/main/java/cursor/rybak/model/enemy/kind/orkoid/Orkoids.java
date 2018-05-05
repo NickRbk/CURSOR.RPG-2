ed4633086b2504cd3844d33577949b8b313ba447
Nick Rybak <nrybak94@gmail.com>
2018-05-05 18:35:31 +0300

    dev: minor changes

--- src/main/java/cursor/rybak/model/enemy/kind/orkoid/Orkoids.java
+++ src/main/java/cursor/rybak/model/enemy/kind/orkoid/Orkoids.java
@@ -9,8 +9,8 @@ public enum Orkoids implements MinLevel {
     OrkWarlord("Ork Warlord", 150, 50, 40, 7),             // L7-8
     OrkShaman("Ork Shaman", 80, 35, 22, 5),                // L5-6
     Ork("Ork", 100, 30, 18, 3),                            // L3
-    Goblin("Goblin", 35, 20, 10, 1),                       // L1-2
-    UnstableAlchemist("Unstable Alchemist", 15, 25, 5, 1); // L1-2
+    UnstableAlchemist("Unstable Alchemist", 15, 25, 5, 1), // L1-2
+    Goblin("Goblin", 35, 20, 10, 1);                       // L1-2
 
     private String name;
     private int health;
--- src/main/java/cursor/rybak/store/enemies/count/EnemiesCount.java
+++ src/main/java/cursor/rybak/store/enemies/count/EnemiesCount.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum EnemiesCount {
-    LVL_1(1, 3), LVL_3(3, 4), LVL_5(5, 5), LVL_7(7, 6), LVL_9(9, 7);
+    LVL_1(1, 3), LVL_3(3, 4), LVL_5(5, 5), LVL_7(7, 6), LVL_9(9, 6);
 
     private int minLevel;
     private int totalCount;
